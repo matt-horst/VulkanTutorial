d340e7f509ab2012a2597b87f3e756fc3f009fab
Matt Horst <mattahorst@gmail.com>
2026-03-16 22:19:48 -0600

    adds alignment specification to uniform buffer data struct

--- 23_descriptor_sets.cpp
+++ 23_descriptor_sets.cpp
@@ -19,6 +19,7 @@ import vulkan_hpp;
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -56,9 +57,9 @@ struct Vertex {
 };
 
 struct ShaderData {
-  glm::mat4 model;
-  glm::mat4 view;
-  glm::mat4 projection;
+  alignas(16) glm::mat4 model;
+  alignas(16) glm::mat4 view;
+  alignas(16) glm::mat4 projection;
 };
 
 const std::vector<Vertex> vertices = {
